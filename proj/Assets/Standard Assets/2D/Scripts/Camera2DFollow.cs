0daa63b59a2b00735b007f2eab7ba1fbdc00d190
rafkol <rafalkolasa@o2.pl>
2015-07-02 09:28:18 +0200

    [od RS] dowolna liczba warstw i ich parametrów paralax'owanych

--- proj/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ proj/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -16,11 +16,8 @@ namespace UnityStandardAssets._2D
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
 
-		public Transform backgroundNearNode;
-		public Transform backgroundFarNode;
-
-		public Vector2 backgroundNearRatio = new Vector2(0.1f,0.05f);
-		public Vector2 backgroundFarRatio = new Vector2(0.3f,0.15f);
+		public Transform[] backgroundsNodes;
+		public Vector2[] backgroundsRatios;
 
 		private Vector3 lastPos;
 
@@ -29,15 +26,6 @@ namespace UnityStandardAssets._2D
         private void Start()
         {
 			camera = GetComponent<Camera> ();
-
-			print ("---------------------------------------");
-			print (camera.orthographicSize);
-			float horzExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
-			print (horzExtent);
-
-			print ("---------------------------------------");
-			//m_LastTargetPosition = target.position;
-            //m_OffsetZ = (transform.position - target.position).z;
 
 			transform.position = new Vector3( target.position.x, target.position.y, transform.position.z );
             transform.parent = null;
@@ -60,40 +48,19 @@ namespace UnityStandardAssets._2D
 
 			transform.position = new Vector3( target.position.x, target.position.y, transform.position.z );
 
-			if( backgroundNearNode ){
-				Vector3 pos = backgroundNearNode.position;
-				pos.x = transform.position.x * backgroundNearRatio.x;
-				pos.y = transform.position.y * backgroundNearRatio.y;
-				backgroundNearNode.position = pos;
+			int numberOfBackgrounds = backgroundsNodes.Length;
+
+			if (numberOfBackgrounds != backgroundsRatios.Length) {
+				print ("numberOfBackgrounds != backgroundsRatios.GetLength ()");
+				return;
 			}
 
-			if( backgroundFarNode ){
-				Vector3 pos = backgroundFarNode.position;
-				pos.x = transform.position.x * backgroundFarRatio.x;
-				pos.y = transform.position.y * backgroundFarRatio.y;
-				backgroundFarNode.position = pos;
+			for( int i = 0 ; i < numberOfBackgrounds ; ++i ){
+				Vector3 pos = backgroundsNodes[i].position;
+				pos.x = transform.position.x * backgroundsRatios[i].x;
+				pos.y = transform.position.y * backgroundsRatios[i].y;
+				backgroundsNodes[i].position = pos;
 			}
-
-//            // only update lookahead pos if accelerating or changed direction
-//            float xMoveDelta = (target.position - m_LastTargetPosition).x;
-//
-//            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
-//
-//            if (updateLookAheadTarget)
-//            {
-//                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
-//            }
-//            else
-//            {
-//                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
-//            }
-//
-//            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
-//            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
-//
-//            transform.position = newPos;
-//
-//            m_LastTargetPosition = target.position;
         }
     }
 }
