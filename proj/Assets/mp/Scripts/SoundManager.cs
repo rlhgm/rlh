7ceabef62481afb9d6e185f9af3a8df870084ef5
rafkol <rafalkolasa@o2.pl>
2015-07-10 11:09:40 +0200

    dzwięki i muzyka (I)

---
+++ proj/Assets/mp/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource soundSource;
+	public static SoundManager instance = null;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlaySound(AudioClip clip){
+		soundSource.clip = clip;
+		soundSource.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
