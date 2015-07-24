21d7b928fb1ba53dfcdac84c02ab81cd54061389
rafkol <rafalkolasa@o2.pl>
2015-07-24 03:38:50 +0200

    Brakujace pliki do poprzedniego punktu

---
+++ proj/Assets/mp/Scripts/ShowInfoTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowInfoTrigger : MonoBehaviour {
+
+	public string Info = "";
+	public float ShowDuration = 1f;
+	public bool OnlyFirstTime = true;
+	public bool used = false;
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
