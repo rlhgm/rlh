4e5c98a7e63f9d3669eeddb6477fa9f56b398b4f
rafkol <rafalkolasa@o2.pl>
2015-08-04 15:06:00 +0200

    komiks (II)
    + Prefabs/Comic_page.prefab
    + Scripts/ComicPage.cs
    + inne
    - jeszcze nie gotowe

---
+++ proj/Assets/mp/Scripts/ComicPage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComicPage : MonoBehaviour {
+
+	public Sprite spritePart = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
