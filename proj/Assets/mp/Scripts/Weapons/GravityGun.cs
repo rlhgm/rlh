d1338ec631d05938ca4887bbc7c56745847141d8
rafkol <rafalkolasa@o2.pl>
2015-09-08 20:20:49 +0200

    GravityGun: obracanie trzymanego kamienia klawiszami Z i X ale teraz to jest raczej do bani bo zmieniam rotation i obiekt drga bo jego centermass jest nie w piwocie ... przerobie na modyfikacje predkoscia

--- proj/Assets/mp/Scripts/Weapons/GravityGun.cs
+++ proj/Assets/mp/Scripts/Weapons/GravityGun.cs
@@ -37,14 +37,6 @@ public class GravityGun : Weapon {
 			player.ggpsTransform.Rotate( Vector3.forward, deltaTime * 360f );
 		}
 
-//		for (int i = 0 ; i < droppedStones.Count; ++i) {
-//			Rigidbody2D rb = droppedStones[i];
-//			if( rb.IsSleeping() ){
-//				Debug.Log ( "remove dropped stone: " + rb ); 
-//				droppedStones.Remove(rb);
-//			}
-//		}
-
 		if (!Input.GetMouseButton (0)) {
 			if (draggedStone == null) {
 
@@ -89,9 +81,7 @@ public class GravityGun : Weapon {
 		if (Input.GetMouseButtonDown (0)) {
 			
 			draggedStone = null;
-			
-			//lastToMoveDist.Set(0f,0f,0f);
-			
+
 			Vector3 mouseInScene = player.touchCamera.ScreenToWorldPoint(Input.mousePosition);
 			
 			RaycastHit2D hit = Physics2D.Linecast( mouseInScene, mouseInScene, layerIdGroundMoveableMask );
@@ -107,29 +97,10 @@ public class GravityGun : Weapon {
 				} else {
 					draggedStone = null;
 				}
-
-//				if( draggedStone ){
-//					Rigidbody2D tsrb = draggedStone.GetComponent<Rigidbody2D>();
-//					if( tsrb ){
-//						tsrb.gravityScale = 0f;
-//					}else{
-//						draggedStone = null;
-//					}
-//				}
 			}
 		}
 		
 		if (Input.GetMouseButtonUp (0)) {
-			
-//			if( draggedStone ){
-//				Rigidbody2D tsrb = draggedStone.GetComponent<Rigidbody2D>();
-//				if( tsrb ){
-//					draggedStone.GetComponent<Rigidbody2D>().gravityScale = 1f;
-//					draggedStone.GetComponent<Rigidbody2D>().AddForce( lastToMoveDist, ForceMode2D.Impulse );
-//				}
-//				draggedStone = null;
-//			}
-
 			releaseStone();
 		}
 	}
@@ -149,8 +120,42 @@ public class GravityGun : Weapon {
 	//Vector2 S; 			// polozenie
 	//RLHOptionsWindow.
 
+	public static float userStoneRotateSpeed = 180f;
+
 	public override void FUpdate (float fDeltaTime) {
 		//Debug.Log ("FUpdate : " + fDeltaTime);
+
+		if( draggedStone ){ 
+
+			if( Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) ||
+			   Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.X) ){
+
+				Rigidbody2D rb = draggedStone.GetComponent<Rigidbody2D>();
+				if( rb ){
+					rb.angularVelocity = 0;
+				}
+			}
+
+			if( Input.GetKey(KeyCode.Z) ){ // obracam kamien w lewo ...
+
+				Rigidbody2D rb = draggedStone.GetComponent<Rigidbody2D>();
+				if( rb ){
+					//if( rb.angularVelocity < 180 )
+					//	rb.angularVelocity += 1;
+					rb.rotation += ( fDeltaTime * userStoneRotateSpeed );
+				}
+			}
+			else if( Input.GetKey(KeyCode.X) ){ // albo w prawo
+
+				Rigidbody2D rb = draggedStone.GetComponent<Rigidbody2D>();
+				if( rb ){
+					//if( rb.angularVelocity > -180 )
+					//	rb.angularVelocity -= 1;
+					rb.rotation -= ( fDeltaTime * userStoneRotateSpeed );
+				}
+
+			}
+		}
 
 		Vector3 currentMousePosition = Input.mousePosition;
 		//if (currentMousePosition != lastMousePosition) {
