b1efed4b6f4d332c45f038dfab57fdb00066289e
rafkol <rafalkolasa@o2.pl>
2015-09-22 18:19:42 +0200

    porzadki na test3 + gui do broni (I)

---
+++ proj/Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		SpriteRenderer sr = transform.GetComponent<SpriteRenderer> ();
+		if (sr) {
+
+			//sr.sprite.
+			sr.material.SetTextureScale("_MainTex", new Vector2(20f,20f));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
