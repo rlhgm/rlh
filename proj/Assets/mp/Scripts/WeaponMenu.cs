ceb659a95081b5387191520ed49cbc24d76e89f6
rafkol <rafalkolasa@o2.pl>
2015-09-22 19:19:14 +0200

    gui c.d.

---
+++ proj/Assets/mp/Scripts/WeaponMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponMenu : MonoBehaviour {
+
+	public WeaponMenuItem itemKnife;
+	public WeaponMenuItem itemGravityGun;
+
+	// Use this for initialization
+	void Start () {
+		itemKnife = transform.Find ("weaponicon_machete").GetComponent<WeaponMenuItem>();
+		itemGravityGun = transform.Find ("weaponicon_gravitygun").GetComponent<WeaponMenuItem>();
+
+		//print (itemKnife);
+		//print (itemGravityGun);
+
+		itemKnife.setState (WeaponMenuItem.State.OFF);
+		itemGravityGun.setState (WeaponMenuItem.State.OFF);
+
+		itemGravityGun.setState (WeaponMenuItem.State.BLINK);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print("weapon menu update");
+
+	}
+}
---
+++ proj/Assets/mp/Scripts/WeaponMenuItem.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WeaponMenuItem : MonoBehaviour {
+
+	Image blinkSprite;
+	Image sprite;
+
+	void setOpactity(Image target, float newOpacity){
+		//print ("setOpacity : " + newOpacity);
+		Color targetColor = target.color;
+		targetColor.a = newOpacity;
+		target.color = targetColor;
+	}
+	float currentStateTime;
+
+	public enum State{
+		UNDEF = 0,
+		OFF,
+		ON,
+		FADE_IN,
+		FADE_OUT,
+		BLINK
+	};
+
+	public State state;
+
+	public void setState(State newState, float duration = 0f){
+		switch (newState) {
+		case State.ON:
+			setOpactity(sprite,1.0f);
+			setOpactity(blinkSprite,0.0f);
+			break;
+
+		case State.OFF:
+			setOpactity(sprite,0.5f);
+			setOpactity(blinkSprite,0.0f);
+			break;
+
+		case State.FADE_IN:
+			break;
+
+		case State.FADE_OUT:
+			break;
+
+		case State.BLINK:
+			setOpactity(sprite,1.0f);
+			setOpactity(blinkSprite,0.0f);
+			break;
+		};
+
+		currentStateTime = 0f;
+		state = newState;
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentStateTime = 0f;
+		sprite = transform.GetComponent<Image>();
+		blinkSprite = transform.Find ("blink").GetComponent<Image>();
+		setState (State.OFF);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case State.BLINK:
+			setOpactity( blinkSprite, (Mathf.Sin(currentStateTime * Mathf.PI)+1f) * 0.5f );
+			break;
+		}
+
+		currentStateTime += Time.deltaTime;
+	}
+
+//	public void blinkOn(){
+//	}
+//
+//	public void blinkOff(){
+//		
+//	}
+}
