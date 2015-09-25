c6d91ae53891579822a0091a3c3b34371863d9f7
rafkol <rafalkolasa@o2.pl>
2015-09-25 18:21:41 +0200

    3) Przedmioty aktywacje i zbieranie (IV) - przerobilem pickable... aby dalo sie je resetowac po smierci zapa

---
+++ proj/Assets/mp/Scripts/Pickable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickable : MonoBehaviour {
+
+	public enum Type{
+		KNIFE,
+		GRAVITY_GUN
+	}
+
+	public Type type = Type.KNIFE;
+	public bool isActive = true;
+
+	// Use this for initialization
+	void Start () {
+		soh ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void activate(){
+		isActive = true;
+		soh ();
+	}
+
+	public void deactivate(){
+		isActive = false;
+		soh ();
+	}
+
+	void soh(){
+		GetComponent<SpriteRenderer> ().enabled = isActive;
+	}
+}
