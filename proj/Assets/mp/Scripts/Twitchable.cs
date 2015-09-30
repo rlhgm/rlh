2907c6ec2db01476778e236d1c0ea4350714fbbc
rafal <rafalkolasa@o2.pl>
2015-10-01 00:21:49 +0200

    Twitchable: uzywam Time.time zamiast zliczac czas...

--- proj/Assets/mp/Scripts/Twitchable.cs
+++ proj/Assets/mp/Scripts/Twitchable.cs
@@ -5,7 +5,7 @@ public class Twitchable : MonoBehaviour {
 
     Vector3 startPositon;
     Vector3 currentPosition;
-    float time = 0f;
+    //float time = 0f;
 
     public Vector2 TwithRange = new Vector2(1f, 1f);
     public Vector2 TwithTime = new Vector2(1f, 1f);
@@ -15,25 +15,25 @@ public class Twitchable : MonoBehaviour {
     void Start()
     {
         startPositon = transform.position;
-        time = 0f;
+        //time = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        //time += Time.deltaTime;
         currentPosition = startPositon;
 
         if (TwithTime.x > 0f && TwithRange.x > 0f)
         {
-            float _dtx = ((time - Delay.x) / TwithTime.x) * Mathf.PI * 2f;
+            float _dtx = ((Time.time - Delay.x) / TwithTime.x) * Mathf.PI * 2f;
             float dtx = Mathf.Sin(_dtx);
             currentPosition.x += dtx * TwithRange.x;
         }
 
         if (TwithTime.y > 0f && TwithRange.y > 0f)
         {
-            float _dty = ((time - Delay.y) / TwithTime.y) * Mathf.PI * 2f;
+            float _dty = ((Time.time - Delay.y) / TwithTime.y) * Mathf.PI * 2f;
             float dty = Mathf.Sin(_dty);
             currentPosition.y += dty * TwithRange.y;
         }
