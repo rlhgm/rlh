9ac5e0b70562acb16cad8e1252d8c5516a342f75
rafal <rafalkolasa@o2.pl>
2015-10-15 03:37:47 +0200

    dalsze prace

--- proj/Assets/mp/Scripts/Zap/ZapControllers/ZapControllerNormal.cs
+++ proj/Assets/mp/Scripts/Zap/ZapControllers/ZapControllerNormal.cs
@@ -2986,60 +2986,54 @@ public class ZapControllerNormal : ZapController
                 }
             }
 
-            if (fromGround)
+
+            Vector2 _handle = new Vector2();
+            Vector2 ro = transform.position;
+            ro.y += 2f;
+            hit = Physics2D.Raycast(ro, Vector2.right, 1.0f, zap.layerIdGroundMoveableMask);
+            if (hit.collider)
             {
-                Vector2 _handle = new Vector2();
-                Vector2 ro = transform.position;
-                ro.y += 2f;
-                hit = Physics2D.Raycast(ro, Vector2.right, 1.0f, zap.layerIdGroundMoveableMask);
-                if (hit.collider)
+                ro.x += 0.4f;
+                if (hit.collider.GetComponent<GroundMoveable>().handleTouched(ro, ref _handle))
                 {
-                    ro.x += 0.4f;
-                    if( hit.collider.GetComponent<GroundMoveable>().handleTouched(ro,ref _handle))
+                    //Debug.Log(_handle);
+                    // tu takie zabezpieczenie dodatkowe aby nie lapal sie od razu tego co ma pod reka
+                    bool _canCatch = true;
+                    if ((lastCatchedClimbHandle == hit.collider.gameObject))
+                    { //{ && zap.velocity.y >= 0.0f) {
+                        _canCatch = false;
+                    }
+
+                    if (_canCatch)
                     {
-                        //Debug.Log(_handle);
-                        // tu takie zabezpieczenie dodatkowe aby nie lapal sie od razu tego co ma pod reka
-                        bool _canCatch = true;
-                        if ((lastCatchedClimbHandle == hit.collider.gameObject))
-                        { //{ && zap.velocity.y >= 0.0f) {
-                            _canCatch = false;
-                        }
+                        catchedClimbHandle = hit.collider.gameObject;
 
-                        if (_canCatch)
+                        Vector3 handlePos = _handle; //catchedClimbHandle.transform.position;
+                        Vector3 newPos = new Vector3();
+                        newPos.x = handlePos.x - zap.getMyHalfWidth();
+                        newPos.y = handlePos.y - 2.4f; //myHeight;
+
+                        canPullUp = canClimbPullUp2();
+
+                        if (canPullUp)
                         {
-                            catchedClimbHandle = hit.collider.gameObject;
-
-                            Vector3 handlePos = _handle; //catchedClimbHandle.transform.position;
-                            Vector3 newPos = new Vector3();
-                            newPos.x = handlePos.x - zap.getMyHalfWidth();
-                            newPos.y = handlePos.y - 2.4f; //myHeight;
-
-                            canPullUp = canClimbPullUp2();
-
-                            if (canPullUp)
-                            {
-                            }
-
-                            zap.velocity.x = 0.0f;
-                            zap.velocity.y = 0.0f;
-
-                            climbBeforePos = transform.position;
-                            climbAfterPos = newPos;
-                            climbAfterPos2 = handlePos;
-                            climbDistToClimb = climbAfterPos - climbBeforePos;
-                            climbToJumpDuration = climbDistToClimb.magnitude * _speed;
-
-                            zap.setState(Zap.State.CLIMB);
-                            setAction(Action.CLIMB_JUMP_TO_CATCH);
-                            lastFrameHande = false;
-                            return true;
                         }
+
+                        zap.velocity.x = 0.0f;
+                        zap.velocity.y = 0.0f;
+
+                        climbBeforePos = transform.position;
+                        climbAfterPos = newPos;
+                        climbAfterPos2 = handlePos;
+                        climbDistToClimb = climbAfterPos - climbBeforePos;
+                        climbToJumpDuration = climbDistToClimb.magnitude * _speed;
+
+                        zap.setState(Zap.State.CLIMB);
+                        setAction(Action.CLIMB_JUMP_TO_CATCH);
+                        lastFrameHande = false;
+                        return true;
                     }
                 }
-            }
-            else
-            {
-
             }
 
             lastHandlePos = zap.sensorHandleR2.position;
@@ -3101,6 +3095,55 @@ public class ZapControllerNormal : ZapController
                     lastFrameHande = false;
 
                     return true;
+                }
+            }
+
+            Vector2 _handle = new Vector2();
+            Vector2 ro = transform.position;
+            ro.y += 2f;
+            hit = Physics2D.Raycast(ro, Vector2.left, 1.0f, zap.layerIdGroundMoveableMask);
+            if (hit.collider)
+            {
+                ro.x -= 0.4f;
+                if (hit.collider.GetComponent<GroundMoveable>().handleTouched(ro, ref _handle))
+                {
+                    //Debug.Log(_handle);
+                    // tu takie zabezpieczenie dodatkowe aby nie lapal sie od razu tego co ma pod reka
+                    bool _canCatch = true;
+                    if ((lastCatchedClimbHandle == hit.collider.gameObject))
+                    { //{ && zap.velocity.y >= 0.0f) {
+                        _canCatch = false;
+                    }
+
+                    if (_canCatch)
+                    {
+                        catchedClimbHandle = hit.collider.gameObject;
+
+                        Vector3 handlePos = _handle; //catchedClimbHandle.transform.position;
+                        Vector3 newPos = new Vector3();
+                        newPos.x = handlePos.x + zap.getMyHalfWidth();
+                        newPos.y = handlePos.y - 2.4f; //myHeight;
+
+                        canPullUp = canClimbPullUp2();
+
+                        if (canPullUp)
+                        {
+                        }
+
+                        zap.velocity.x = 0.0f;
+                        zap.velocity.y = 0.0f;
+
+                        climbBeforePos = transform.position;
+                        climbAfterPos = newPos;
+                        climbAfterPos2 = handlePos;
+                        climbDistToClimb = climbAfterPos - climbBeforePos;
+                        climbToJumpDuration = climbDistToClimb.magnitude * _speed;
+
+                        zap.setState(Zap.State.CLIMB);
+                        setAction(Action.CLIMB_JUMP_TO_CATCH);
+                        lastFrameHande = false;
+                        return true;
+                    }
                 }
             }
 
