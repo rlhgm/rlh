c3096b62e4bc9e371ee0e8d14f1d4dec0f850e1f
rafal <rafalkolasa@o2.pl>
2015-10-23 17:39:29 +0200

    RLHScene ustawiac w awake’u pozycje na 0,0,0

---
+++ proj/Assets/Materials/RLHScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RLHScene : MonoBehaviour
+{
+    void Awake()
+    {
+        transform.position = new Vector3(0f,0f,0f);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
