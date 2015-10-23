f6e5183e27bb8394e39c2e21af2227469d71a20c
rafal <rafalkolasa@o2.pl>
2015-10-23 14:26:50 +0200

    wyskakujący pickabl z kamienia (I) + sporo przerobek w prefabach - glownie kolizje triggerow z kamieniami

---
+++ proj/Assets/mp/Scripts/SmashStoneActivator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmashStoneActivator : MonoBehaviour
+{
+    /// <summary>
+    /// bla bla bal
+    /// </summary>
+    public Collider2D targetStone;
+    public Pickable pickablePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //print("ssa::OnTriggerEnter2D(Collider2D other)");
+        if( other == targetStone )
+        {
+            //print("ssa::OnTriggerEnter2D(Collider2D other)");
+            if( pickablePrefab )
+            {
+                Pickable newPickable = Instantiate<Pickable>(pickablePrefab);
+                Vector3 startPos = transform.position;
+                newPickable.transform.position = startPos;
+
+                //DestroyObject()
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //print("ssa::OnTriggerStay2D(Collider2D other)");
+
+        //if (isDead())
+        //    return;
+
+        //int lid = other.transform.gameObject.layer;
+        //if (lid == LayerMask.NameToLayer("GroundMoveable"))
+        //{ // layerIdGroundMoveableMask) { // to jest kamien 
+        //    if (hitByStone(other.transform))
+        //    {
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        //touchStone(other.transform);
+        //    }
+        //    return;
+        //}
+    }
+}
