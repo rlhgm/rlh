84c981da52e8dff42d80dd0e59daf89fa48cca01
rafal <rafalkolasa@o2.pl>
2015-11-12 01:35:28 +0100

    SecretPlace - gotowe

--- proj/Assets/mp/Scripts/SecretPlace.cs
+++ proj/Assets/mp/Scripts/SecretPlace.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SecretPlace : MonoBehaviour {
+public class SecretPlace : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Zap>())
+        {
+            setChildenOpacity(transform, 0.3f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Zap>())
+        {
+            setChildenOpacity(transform, 1.0f);
+        }
+    }
+
+    void setChildenOpacity(Transform parent, float newOpacity)
+    {
+        SpriteRenderer renderer = parent.GetComponent<SpriteRenderer>();
+        if (renderer)
+        {
+            Color oldColor = renderer.color;
+            oldColor.a = newOpacity;
+            renderer.color = oldColor;
+        }
+
+        for( int i = 0; i < parent.childCount; ++i )
+        {
+            setChildenOpacity(parent.GetChild(i), newOpacity);
+        }
+    }
 }
