cbee886da3181b4e577e67e376124c20472da052
rafal <rafalkolasa@o2.pl>
2015-11-13 18:22:03 +0100

    nietoperz: lata ...
    ... ósemki, zmiana kierunków

--- proj/Assets/mp/Scripts/Enemies/Bat.cs
+++ proj/Assets/mp/Scripts/Enemies/Bat.cs
@@ -9,35 +9,84 @@ public class Bat : MonoBehaviour
     Vector3 quaverPos = new Vector3(0f, 0f, 0f);
     bool quavering = false;
 
+    float TurnbackDuration = 0.5f;
+
+    void Awake()
+    {
+        StaticInit();
+        myGfx = transform.Find("gfx");
+        myAnimator = myGfx.GetComponent<Animator>();
+    }
+
     // Use this for initialization
     void Start()
     {
         //quaverBegin();
+        lastPos = transform.position;
+        velocity = new Vector2(0f, 0f);
+        lastVelocity = new Vector2(0f, 0f);
+        newVelocity = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (quavering) quaverStep(Time.deltaTime);
-        else quaverBegin();
+        currentDeltaTime = Time.deltaTime;
+        currentActionTime += currentDeltaTime;
+
+        lastVelocity = velocity;
+        lastPos = transform.position;
+
+        switch (action)
+        {
+            case Action.Turnback:
+                ActionTurnback();
+                break;
+        }
+
+        if (quavering) QuaverStep();
+        else QuaverBegin();
+
+        CalculateVelocity();
+        //print(velocity);
+        TurnAccordingVelocity();
     }
 
-    void quaverBegin()
+    void CalculateVelocity()
+    {
+        _pos = transform.position - lastPos;
+        velocity = _pos / currentDeltaTime;
+    }
+    void TurnAccordingVelocity()
+    {
+        if (velocity.x > 0f)
+        {
+            if (FaceLeft()) TurnbackStart();
+        }
+        else if (velocity.x < 0f)
+        {
+            if (FaceRight()) TurnbackStart();
+        }
+    }
+    void QuaverBegin()
     {
         quavering = true;
         quaverTime = 0f;
         quaverStartPos = transform.position;
+
+        //print( myAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash );
+        //print(flyAnimStateHash);
     }
 
-    void quaverFinish()
+    void QuaverFinish()
     {
         quavering = false;
     }
 
-    void quaverStep(float deltaTime)
+    void QuaverStep()
     {
         bool _quaverEnded = false;
-        quaverTime += deltaTime;
+        quaverTime += currentDeltaTime;
         if (quaverTime >= quaverDuration)
         {
             quaverTime = quaverDuration;
@@ -52,6 +101,142 @@ public class Bat : MonoBehaviour
         transform.position = quaverStartPos + quaverPos;
 
         if (_quaverEnded)
-            quaverFinish();
+            QuaverFinish();
     }
+
+    Transform myGfx = null;
+    Animator myAnimator = null;
+
+    static bool staticInitiated = false;
+    static int flyAnimStateHash;
+    static int turnbackAnimStateHash;
+    static int attackAnimStateHash;
+
+    static bool StaticInit()
+    {
+        if (staticInitiated) return false;
+
+        flyAnimStateHash = Animator.StringToHash("Fly");
+        turnbackAnimStateHash = Animator.StringToHash("Turnback");
+        attackAnimStateHash = Animator.StringToHash("Attack");
+
+        staticInitiated = true;
+        return true;
+    }
+
+    public enum Action
+    {
+        Undef = 0,
+        Fly,
+        Turnback,
+        Attack
+    };
+
+    Action action;
+
+    bool SetAction(Action newAction, int param = 0)
+    {
+        if (action == newAction)
+            return false;
+
+        actionChangedPos = transform.position;
+        currentActionTime = 0f;
+        actionJustChanged = true;
+        action = newAction;
+
+        myAnimator.speed = 1f;
+
+        //myAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+
+        switch (action)
+        {
+            case Action.Fly:
+                myAnimator.Play(flyAnimStateHash);
+                break;
+
+            case Action.Turnback:
+                myAnimator.Play(turnbackAnimStateHash);
+                break;
+
+            case Action.Attack:
+                myAnimator.Play(attackAnimStateHash);
+                break;
+        }
+
+        return true;
+    }
+
+    public bool IsInAction(Action test)
+    {
+        return action == test;
+    }
+
+    public bool IsNotInAction(Action test)
+    {
+        return action != test;
+    }
+
+    void ActionTurnback()
+    {
+        if (currentActionTime >= TurnbackDuration)
+        {
+            Turnback();
+            SetAction(Action.Fly);
+        }
+    }
+
+    void TurnbackStart()
+    {
+        SetAction(Action.Turnback);
+    }
+    void Turnback()
+    {
+        if (FaceRight()) TurnLeft();
+        else TurnRight();
+    }
+    public void TurnLeft()
+    {
+        Vector3 scl = myGfx.localScale;
+        scl.x = Mathf.Abs(scl.x) * 1.0f;
+        myGfx.localScale = scl;
+    }
+    public void TurnRight()
+    {
+        Vector3 scl = myGfx.localScale;
+        scl.x = Mathf.Abs(scl.x) * -1.0f;
+        myGfx.localScale = scl;
+    }
+
+    public Vector2 Dir()
+    {
+        return myGfx.localScale.x < 0.0f ? Vector2.right : -Vector2.right;
+    }
+    public Vector2 DirN()
+    {
+        return myGfx.localScale.x < 0.0f ? -Vector2.right : Vector2.right;
+    }
+    public int Dir2()
+    {
+        return myGfx.localScale.x < 0f ? (int)1f : (int)-1f;
+    }
+
+    public bool FaceLeft()
+    {
+        return myGfx.localScale.x > 0f;
+    }
+    public bool FaceRight()
+    {
+        return myGfx.localScale.x < 0f;
+    }
+
+    Vector2 velocity = new Vector2(0f, 0f);
+    Vector2 lastVelocity = new Vector2(0f, 0f);
+    Vector2 newVelocity = new Vector2(0f, 0f);
+    Vector3 lastPos = new Vector3(0f, 0f, 0f);
+    Vector3 _pos = new Vector3(0f, 0f, 0f);
+
+    Vector3 actionChangedPos = new Vector3(0f, 0f, 0f);
+    float currentDeltaTime = 0.0f;
+    float currentActionTime = 0.0f;
+    bool actionJustChanged = false;
 }
