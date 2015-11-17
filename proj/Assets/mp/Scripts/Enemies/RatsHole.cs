e941fbb6d16f24d0b1549429efec674a528d6122
rafal <rafalkolasa@o2.pl>
2015-11-17 23:12:04 +0100

    funkcje testujace czy wejscie i wyjscie nie sa zastawione + scena do testów

--- proj/Assets/mp/Scripts/Enemies/RatsHole.cs
+++ proj/Assets/mp/Scripts/Enemies/RatsHole.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class RatsHole : MonoBehaviour
 {
     public RatsHole ExitHole = null;
-
+    
     // Use this for initialization
     void Start()
     {
@@ -12,11 +12,68 @@ public class RatsHole : MonoBehaviour
         {
             Debug.LogError("RatsHole : " + name + " nie ma zdefiniowanego wyjscia!");
         }
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        exitBoxCollider = ExitHole.GetComponent<BoxCollider2D>();
+        findWorldVertices();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    BoxCollider2D boxCollider;
+    Vector2 topLeft;
+    Vector2 topRight;
+    Vector2 btmLeft;
+    Vector2 btmRight;
+    BoxCollider2D exitBoxCollider;
+    Vector2 exitTopLeft;
+    Vector2 exitTopRight;
+    Vector2 exitBtmLeft;
+    Vector2 exitBtmRight;
+    RaycastHit2D hit;
+
+    public bool EntryIsOpen()
+    {
+        hit = Physics2D.Linecast(btmLeft, topRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
+        hit = Physics2D.Linecast(topLeft, btmRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
+        return true;
+    }
+
+
+    public bool ExitIsOpen()
+    {
+        hit = Physics2D.Linecast(exitBtmLeft, exitTopRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
+        hit = Physics2D.Linecast(exitTopLeft, exitBtmRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
+        return true;
+    }
+
+    void findWorldVertices()
+    {
+        float top = exitBoxCollider.offset.y + (exitBoxCollider.size.y * 0.5f);
+        float btm = exitBoxCollider.offset.y - (exitBoxCollider.size.y * 0.5f);
+        float left = exitBoxCollider.offset.x - (exitBoxCollider.size.x * 0.5f);
+        float right = exitBoxCollider.offset.x + (exitBoxCollider.size.x * 0.5f);
+
+        exitTopLeft = transform.TransformPoint(new Vector3(left, top, 0f));
+        exitTopRight = transform.TransformPoint(new Vector3(right, top, 0f));
+        exitBtmLeft = transform.TransformPoint(new Vector3(left, btm, 0f));
+        exitBtmRight = transform.TransformPoint(new Vector3(right, btm, 0f));
+
+        top = boxCollider.offset.y + (boxCollider.size.y * 0.5f);
+        btm = boxCollider.offset.y - (boxCollider.size.y * 0.5f);
+        left = boxCollider.offset.x - (boxCollider.size.x * 0.5f);
+        right = boxCollider.offset.x + (boxCollider.size.x * 0.5f);
+
+        topLeft = transform.TransformPoint(new Vector3(left, top, 0f));
+        topRight = transform.TransformPoint(new Vector3(right, top, 0f));
+        btmLeft = transform.TransformPoint(new Vector3(left, btm, 0f));
+        btmRight = transform.TransformPoint(new Vector3(right, btm, 0f));
     }
 }
