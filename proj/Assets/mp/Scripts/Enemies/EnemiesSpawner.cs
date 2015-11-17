d88891e49066a0ba54a42ec9c06114502e5e8ce0
rafal <rafalkolasa@o2.pl>
2015-11-17 19:26:52 +0100

    zamykanie spawnera kamieniami dziala

--- proj/Assets/mp/Scripts/Enemies/EnemiesSpawner.cs
+++ proj/Assets/mp/Scripts/Enemies/EnemiesSpawner.cs
@@ -5,15 +5,23 @@ using System.Collections.Generic;
 public class EnemiesSpawner : MonoBehaviour
 {
     public Enemy EnemyPrefab = null;
-    public Vector2 SpawnIntervalMinMax = new Vector2(1f,5f);
+    public Vector2 SpawnIntervalMinMax = new Vector2(1f, 5f);
     public int MaxSpawned = 3;
 
     float toSpawnTimer = 0.0f;
     List<Enemy> spawnedEnemies;
+    BoxCollider2D boxCollider;
+
+    Vector2 topLeft;
+    Vector2 topRight;
+    Vector2 btmLeft;
+    Vector2 btmRight;
 
     // Use this for initialization
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
+        findWorldVertices();
         spawnedEnemies = new List<Enemy>(MaxSpawned);
         ResetSpawTimer();
     }
@@ -44,10 +52,32 @@ public class EnemiesSpawner : MonoBehaviour
         return true;
     }
 
+    RaycastHit2D hit;
     bool CheckExitIsOpen()
     {
+        hit = Physics2D.Linecast(btmLeft, topRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
+        hit = Physics2D.Linecast(topLeft, btmRight, RLHScene.Instance.layerIdGroundAllMask);
+        if (hit.collider) return false;
         return true;
-    } 
+    }
+
+    public void findWorldVertices()
+    {
+        float top = boxCollider.offset.y + (boxCollider.size.y * 0.5f);
+        float btm = boxCollider.offset.y - (boxCollider.size.y * 0.5f);
+        float left = boxCollider.offset.x - (boxCollider.size.x * 0.5f);
+        float right = boxCollider.offset.x + (boxCollider.size.x * 0.5f);
+
+        topLeft = transform.TransformPoint(new Vector3(left, top, 0f));
+        topRight = transform.TransformPoint(new Vector3(right, top, 0f));
+        btmLeft = transform.TransformPoint(new Vector3(left, btm, 0f));
+        btmRight = transform.TransformPoint(new Vector3(right, btm, 0f));
+
+        //Debug.Log(topLeft + " " + topRight + " " + btmLeft + " " + btmRight);
+
+        //float rot = transform.rotation.eulerAngles.z;
+    }
 
     void ResetSpawTimer()
     {
@@ -56,7 +86,7 @@ public class EnemiesSpawner : MonoBehaviour
 
     public void NurslingFall(Enemy zombie)
     {
-        Destroy(zombie.gameObject,2f);
+        Destroy(zombie.gameObject, 2f);
         spawnedEnemies.RemoveAt(spawnedEnemies.IndexOf(zombie));
     }
 
@@ -75,4 +105,9 @@ public class EnemiesSpawner : MonoBehaviour
         spawnedEnemies.Clear();
         ResetSpawTimer();
     }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    print(other.name);
+    //}
 }
