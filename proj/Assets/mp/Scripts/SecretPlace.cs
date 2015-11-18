8b17a22638726dc1897b2ce39228fce3537d7715
rafal <rafalkolasa@o2.pl>
2015-11-18 01:20:03 +0100

    SecretPlace: płynne zanikanie/pojawianie sie

--- proj/Assets/mp/Scripts/SecretPlace.cs
+++ proj/Assets/mp/Scripts/SecretPlace.cs
@@ -4,33 +4,52 @@ using System.Collections;
 public class SecretPlace : MonoBehaviour
 {
     public float RevealOpacity = 0.3f;
-
+    public float RevealOpacitySpeed = 0.5f;
+    float currentOpacity = 1f;
+    float targetOpacity = 1f;
+    
     // Use this for initialization
     void Start()
     {
-
+        //startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if( currentOpacity != targetOpacity )
+        {
+            if( currentOpacity > targetOpacity )
+            {
+                currentOpacity = Mathf.Max(currentOpacity - RevealOpacitySpeed * Time.deltaTime,targetOpacity);
+            }
+            else
+            {
+                currentOpacity = Mathf.Min(currentOpacity + RevealOpacitySpeed * Time.deltaTime, targetOpacity);
+            }
+            setChildenOpacity(transform, currentOpacity);
+            //currentOpacity = targetOpacity;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<Zap>())
-        {
-            setChildenOpacity(transform, RevealOpacity);
-        }
+        targetOpacity = RevealOpacity;
+
+        //if (other.GetComponent<Zap>())
+        //{
+        //    setChildenOpacity(transform, RevealOpacity);
+        //}
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<Zap>())
-        {
-            setChildenOpacity(transform, 1.0f);
-        }
+        targetOpacity = 1f;
+
+        //if (other.GetComponent<Zap>())
+        //{
+        //    setChildenOpacity(transform, 1.0f);
+        //}
     }
 
     void setChildenOpacity(Transform parent, float newOpacity)
