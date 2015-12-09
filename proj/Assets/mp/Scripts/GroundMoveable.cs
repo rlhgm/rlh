7890f45329016b03e631844e541db00ef34e695d
rafal <rafalkolasa@o2.pl>
2015-12-09 22:41:22 +0100

    wykrywanie aktywnych uchwytow na nowych kamieniach

--- proj/Assets/mp/Scripts/GroundMoveable.cs
+++ proj/Assets/mp/Scripts/GroundMoveable.cs
@@ -13,9 +13,11 @@ public class GroundMoveable : MonoBehaviour
     //NewRope connectedRope;
 
     Vector2[] handles;
+    bool[] handlesActive;
     Vector2[] normals;
 
     bool lastSleeped = false;
+    bool isClockwise = false;
 
     void Awake()
     {
@@ -28,20 +30,24 @@ public class GroundMoveable : MonoBehaviour
             PolygonCollider2D coll = GetComponent<PolygonCollider2D>();
             normals = new Vector2[coll.points.Length];
             handles = new Vector2[coll.points.Length];
-            updateWorldNormalsAndHandles(coll);
+            handlesActive = new bool[coll.points.Length];
+            isClockwise = PolygonIsClockwise(coll.points);
+            UpdateWorldNormalsAndHandles(coll);
         }
         else if(GetComponent<BoxCollider2D>())
         {
             BoxCollider2D coll = GetComponent<BoxCollider2D>();
             normals = new Vector2[4];
             handles = new Vector2[4];
-            updateWorldNormalsAndHandles(coll);
+            handlesActive = new bool[4];
+            UpdateWorldNormalsAndHandles(coll);
         }
         else
         {
             Debug.LogError("GroundMoveable nie ma colidera");
         }
 
+        
         //lastSleeping = false;
 
         SaveResets();
@@ -63,18 +69,18 @@ public class GroundMoveable : MonoBehaviour
             {
                 if (GetComponent<PolygonCollider2D>())
                 {
-                    updateWorldNormalsAndHandles(GetComponent<PolygonCollider2D>());
+                    UpdateWorldNormalsAndHandles(GetComponent<PolygonCollider2D>());
                 }
                 else if (GetComponent<BoxCollider2D>())
                 {
-                    updateWorldNormalsAndHandles(GetComponent<BoxCollider2D>());
+                    UpdateWorldNormalsAndHandles(GetComponent<BoxCollider2D>());
                 }
                 else
                 {
                     Debug.LogError("GroundMoveable nie ma colidera");
                 }
             }
-            drawEdgesAndNormals();
+            DrawEdgesAndNormals();
         }
 
         lastSleeped = physic.IsSleeping();        
@@ -95,19 +101,20 @@ public class GroundMoveable : MonoBehaviour
         physic.angularVelocity = resetAngularVelocity;
     }
 
-    void updateWorldNormalsAndHandles(PolygonCollider2D coll)
+    void UpdateWorldNormalsAndHandles(PolygonCollider2D coll)
     {
         int i = 0;
         for (; i < coll.points.Length - 1; ++i)
         {
             handles[i] = transform.TransformPoint(coll.points[i]);            
-            addNormal(i, coll.points[i], coll.points[i + 1]);
+            AddNormal(i, coll.points[i], coll.points[i + 1]);
         }
         handles[i] = transform.TransformPoint(coll.points[i]);
-        addNormal(i, coll.points[i], coll.points[0]);
+        AddNormal(i, coll.points[i], coll.points[0]);
+        UpdateHandlesActivated();
     }
 
-    void updateWorldNormalsAndHandles(BoxCollider2D coll)
+    void UpdateWorldNormalsAndHandles(BoxCollider2D coll)
     {
         //int i = 0;
         //for (; i < coll.points.Length - 1; ++i)
@@ -118,67 +125,83 @@ public class GroundMoveable : MonoBehaviour
         //addNormal(i, coll.points[i], coll.points[0]);
     }
 
+    void UpdateHandlesActivated()
+    {
+        int i = 0;
+        float wnAngle = 0.0f;
+        bool b = false;
+
+        for (; i < handlesActive.Length; ++i)
+        {
+            handlesActive[i] = false;
+        }
+        for (i = 0; i < normals.Length - 1; ++i)
+        {
+            wnAngle = Vector2.Angle(Vector2.up, normals[i]);
+            b = Mathf.Abs(wnAngle) < 45.0f;
+            if (b)
+            {
+                handlesActive[i] = true;
+                handlesActive[i + 1] = true;
+            }
+        }
+        wnAngle = Vector2.Angle(Vector2.up, normals[i]);
+        b = Mathf.Abs(wnAngle) < 45.0f;
+        if (b)
+        {
+            handlesActive[i] = true;
+            handlesActive[0] = true;
+        }
+    }
+
     //Vector2 p1;
     //Vector2 p2;
     Vector2 pn1;
     Vector2 pn2;
     Vector2 wn;
-    void addNormal(int index, Vector2 p1, Vector2 p2)
+    void AddNormal(int index, Vector2 p1, Vector2 p2)
     {
-        //Vector2 diff = p2 - p1;
-        //normals[index] = diff.Rotate(90).normalized * 0.25f;
-
-        //Vector2 _p1 = p1 + (p2 - p1) * 0.5f;
-        //pn1 = transform.TransformPoint(_p1);
-        //pn2 = transform.TransformPoint(_p1 + normals[i]);
-        //Debug.DrawLine(pn1, pn2);
-
-        //wn = pn2 - pn1;
-        //float wnAngle = Vector2.Angle(Vector2.up, wn);
-
-        //if (Mathf.Abs(wnAngle) < 45.0f)
-        //{
-        //    Debug.DrawLine(transform.TransformPoint(p1), transform.TransformPoint(p2), Color.red);
-        //}
-
         pn1 = transform.TransformPoint(p1);
         pn2 = transform.TransformPoint(p2);
 
         Vector2 diff = pn2 - pn1;
-        normals[index] = diff.Rotate(90).normalized * 0.25f;
+        normals[index] = diff.Rotate(isClockwise?90:-90).normalized * 0.25f;
 
+        //float wnAngle = Vector2.Angle(Vector2.up, normals[index]);
+        //handlesActive[index] = Mathf.Abs(wnAngle) < 45.0f;
     }
 
-    void drawEdgesAndNormals()
+    void DrawEdgesAndNormals()
     {
-        
         int i = 0;
         for (; i < handles.Length - 1; ++i)
         {
-            drawEdgeAndNormal(handles[i], handles[i + 1]);
+            DrawEdgeAndNormal(handles[i], handles[i + 1], i);
         }
-        drawEdgeAndNormal(handles[i], handles[0]);
+        DrawEdgeAndNormal(handles[i], handles[0], i);
     }
     
-    void drawEdgeAndNormal(Vector2 p1, Vector2 p2)
+    void DrawEdgeAndNormal(Vector2 p1, Vector2 p2, int i)
     {
-        Debug.DrawLine(p1, p2);
+        pn1 = p1 + (p2 - p1) * 0.5f;
+        pn2 = pn1 + normals[i];
+        Debug.DrawLine(pn1, pn2);
 
-        ////p1 = coll.points[p1Ind];
-        ////p2 = coll.points[p2Ind];
-        //Vector2 _p1 = p1 + (p2 - p1) * 0.5f;
-        //pn1 = transform.TransformPoint(_p1);
-        //pn2 = transform.TransformPoint(_p1 + normals[i]);
-        //Debug.DrawLine(pn1, pn2);
-        ////return pn2-pn1;
+        float wnAngle = Vector2.Angle(Vector2.up, normals[i]);
+        if (Mathf.Abs(wnAngle) < 45.0f)
+        {
+            Debug.DrawLine(p1, p2, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(p1, p2, Color.blue);
+        }
 
-        //wn = pn2 - pn1;
-        //float wnAngle = Vector2.Angle(Vector2.up, wn);
-
-        //if (Mathf.Abs(wnAngle) < 45.0f)
-        //{
-        //    Debug.DrawLine(transform.TransformPoint(p1), transform.TransformPoint(p2), Color.red);
-        //}
+        if( handlesActive[i])
+        {
+            Debug.DrawLine(handles[i] + new Vector2(-0.1f, -0.1f), handles[i] + new Vector2(0.1f, 0.1f), Color.red);
+            Debug.DrawLine(handles[i] + new Vector2(-0.1f, 0.1f), handles[i] + new Vector2(0.1f, -0.1f), Color.red);
+        }
     }
 
     //void OnMouseOver()
@@ -205,6 +228,24 @@ public class GroundMoveable : MonoBehaviour
 
     //    //float rot = transform.rotation.eulerAngles.z;
     //}
+
+    bool PolygonIsClockwise(params Vector2[] points)
+    {
+        int l = points.Length;
+
+        float sum = 0f;
+
+        for (int i = 0; i < l; i++)
+        {
+            int n = i + 1 >= l - 1 ? 0 : i + 1;
+
+            float x = points[n].x - points[i].x;
+            float y = points[n].y + points[i].y;
+            sum += (x * y);
+        }
+
+        return (sum < 0) ? false : true;
+    }
 
     public bool handleToPullDownTouched(Vector2 zapDir, Vector3 worldTouch, ref Vector2 handle, float maxTilt = 5f, float maxDist = 0.25f)
     {
