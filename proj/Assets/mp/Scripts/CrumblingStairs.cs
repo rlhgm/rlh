c50b60f7f139d00afe6ac8648f8cb64cd96f289f
rafal <rafalkolasa@o2.pl>
2015-12-18 01:22:23 +0100

    zawalajace sie schody - dalsze prace

---
+++ proj/Assets/mp/Scripts/CrumblingStairs.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrumblingStairs : MonoBehaviour
+{
+    Transform ground = null;
+    Transform handle = null;
+    Transform mount = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        ground = transform.Find("ground");
+        handle = transform.Find("handle");
+        mount = transform.Find("mount");
+
+        if (!ground)
+        {
+            Debug.LogError("CrumblingStairs : " + name + " nie ma ground");
+            Debug.Break();
+        }
+        if (!handle)
+        {
+            Debug.LogError("CrumblingStairs : " + name + " nie ma handle");
+            Debug.Break();
+        }
+        if (!mount)
+        {
+            Debug.LogError("CrumblingStairs : " + name + " nie ma mount");
+            Debug.Break();
+        }
+
+        mount.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Clumble()
+    {
+        ground.gameObject.SetActive(false);
+        handle.gameObject.SetActive(false);
+        mount.gameObject.SetActive(true);
+    }
+
+    public void Reset()
+    {
+
+    }
+}
