5d4bcfc4f0fe09327215f339f684d7268622d022
rafal <rafalkolasa@o2.pl>
2015-12-30 20:56:18 +0100

    zap zasłania promienie (I)

---
+++ proj/Assets/mp/Scripts/SpriteOpacityController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteOpacityController : MonoBehaviour
+{
+    public SpriteRenderer[] sprites;
+    public float[] TargetsAlphas;
+    public bool[] LinearFadeOuts;
+
+    float[] StartsAlphas;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
