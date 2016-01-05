bd5e8eef056fcf7cd9ca8776194fb256e7e0f565
rafal <rafalkolasa@o2.pl>
2016-01-05 20:59:08 +0100

    Trigger - CameraShaker - gotowe

--- proj/Assets/mp/Scripts/CameraShaker.cs
+++ proj/Assets/mp/Scripts/CameraShaker.cs
@@ -3,16 +3,38 @@ using System.Collections;
 
 public class CameraShaker : MonoBehaviour
 {
+    public float ShakeAmplitude = 0.25f;
+    public float ShakeSpeed = 8f;
+    public float ShakeFadeOutDuration = 0.5f;
 
-    // Use this for initialization
-    void Start()
+    //// Use this for initialization
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    void OnTriggerEnter2D(Collider2D collider)
     {
-
+        //print("CameraShaker::OnTriggerEnter2D");
+        //RLHScene.Instance.Zap.CameraTargetOffset = CameraOffset;
+        RLHScene.Instance.CamController.ShakePermanentStart(ShakeAmplitude,ShakeSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit2D()
     {
-
+        //print("CameraShaker::OnTriggerExit2D");
+        //RLHScene.Instance.Zap.CameraTargetOffset = new Vector3(0f, 0f, 0f);
+        RLHScene.Instance.CamController.ShakeStop(ShakeFadeOutDuration);
     }
+
+    //void OnTriggerStay2D()
+    //{
+    //    print("CameraTargetOffseter::OnTriggerStay2D");
+    //}
 }
