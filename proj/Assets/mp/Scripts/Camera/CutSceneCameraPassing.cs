e3ac5804d796f6056ee089f453b99b446fa6a6bd
rafal <rafalkolasa@o2.pl>
2016-01-06 11:59:41 +0100

    porzadek na scenie testowej bo sie zrobil duzy chlewik

--- proj/Assets/mp/Scripts/Camera/CutSceneCameraPassing.cs
+++ proj/Assets/mp/Scripts/Camera/CutSceneCameraPassing.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CutSceneCameraPassing : MonoBehaviour {
+public class CutSceneCameraPassing : MonoBehaviour
+{
+    public CutSceneCameraPassingControlPoint next = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool OneTimeOnly = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        print("CutSceneCameraPassing::OnTriggerEnter2D");
+        //RLHScene.Instance.Zap.CameraTargetOffset = CameraOffset;
+    }
+
+    void OnTriggerExit2D()
+    {
+        print("CutSceneCameraPassing::OnTriggerExit2D");
+        //RLHScene.Instance.Zap.CameraTargetOffset = new Vector3(0f, 0f, 0f);
+    }
+
+    //void OnTriggerStay2D()
+    //{
+    //    print("CameraTargetOffseter::OnTriggerStay2D");
+    //}
 }
--- proj/Assets/mp/Scripts/Camera/CutSceneCameraPassingControlPoint.cs
+++ proj/Assets/mp/Scripts/Camera/CutSceneCameraPassingControlPoint.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CutSceneCameraPassingControlPoint : MonoBehaviour {
+public class CutSceneCameraPassingControlPoint : MonoBehaviour
+{
+    public CutSceneCameraPassingControlPoint next = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (next)
+        {
+            Debug.DrawLine(transform.position, next.transform.position);
+        }
+    }
 }
