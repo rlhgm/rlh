4ea1e453615d9a2e610b44778969c2ed99d69246
rafal <rafalkolasa@o2.pl>
2016-01-07 20:52:17 +0100

    tagowanie terenow skryptem Surface - do dzwiekow i pewnie nie tylko

---
+++ proj/Assets/mp/Scripts/Ground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+    public enum Type
+    {
+        Stone,
+        Wood,
+        Grass
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ proj/Assets/mp/Scripts/Surface.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Surface : MonoBehaviour
+{
+    public enum Type
+    {
+        Default,
+        Ground,
+        Grass,
+        Stone,
+        Wood
+    }
+
+    public Type type = Type.Default;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
