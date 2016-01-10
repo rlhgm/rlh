95d31cf949132516549f5e2612bf2dc63b6a9fed
rafal <rafalkolasa@o2.pl>
2016-01-10 02:30:17 +0100

    AutoDestroyer do obiektów particli

---
+++ proj/Assets/mp/Scripts/AutoDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroyer : MonoBehaviour
+{
+    public float TimeToDestroy = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, TimeToDestroy);
+    }
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+}
